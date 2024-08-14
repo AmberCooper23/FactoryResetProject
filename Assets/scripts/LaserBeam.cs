9fcf139ac98f7eaa07a0f43de6c2c5e47eea992c
timothychilezi20 <timothychilezi@gmail.com>
2024-08-14 17:22:58 +0200

    Lasers and Stuff

---
+++ Assets/scripts/LaserBeam.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeam : MonoBehaviour
+{
+
+    private LineRenderer lr;
+
+    
+    public Transform startPoint;
+
+
+    private void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+
+    void Update()
+    {
+
+        lr.SetPosition(0, startPoint.position);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.right, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, hit.point);
+            }
+            if (hit.transform.tag == "Player")
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+        else lr.SetPosition(1, transform.right * 5000);
+    }
+}
