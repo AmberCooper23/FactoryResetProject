b804a558d5bae0275045a71aafa0e1cb90b96fec
AmberCooper23 <2677396@students.wits.ac.za>
2024-08-16 01:46:14 +0200

    killbox mostly done
    note: we just need to figure out why it takes so long, it might be a frame rate issue and not a coding issue.

--- Assets/scripts/Killbox.cs
+++ Assets/scripts/Killbox.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine;
-
 public class Killbox : MonoBehaviour
 {
-    [SerializeField] private Transform respawnPosition; // Position where the player should respawn
-    [SerializeField] private string playerTag = "Player"; // Tag used to identify the player
+    //[SerializeField] private Transform respawnPosition; // Position where the player should respawn
+    //[SerializeField] private string playerTag = "Player"; // Tag used to identify the player
+    public LevelManager levelManager;
+  //  public GameObject player;
 
     // Called when another collider enters the trigger collider
-    private void OnTriggerEnter(Collider other)
+    void OnCollisionEnter  (Collision other)
     {
-        if (other.CompareTag(playerTag)) // Check if the collider has the specified tag
+        if (other.gameObject.CompareTag("Player")) // Check if the collider has the specified tag
         {
+            Debug.Log("collided with killbox");
             // Respawn the player to the respawn position
-            other.transform.position = respawnPosition.position;
-            // Optionally, reset the player's velocity or other states here if needed
+           levelManager.RespawnPlayer(); // call the Respawn Player method from the Level Manager script
         }
     }
 }
