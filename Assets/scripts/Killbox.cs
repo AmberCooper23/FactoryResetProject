ca3099cd4f36b664b3f3004df379a1366bdab48f
timothychilezi20 <timothychilezi@gmail.com>
2024-08-16 18:40:25 +0200

    Falling Platform Respawn

--- Assets/scripts/Killbox.cs
+++ Assets/scripts/Killbox.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Killbox : MonoBehaviour
 {
+    public GameObject Platform1, Platform2, Platform3, Platform4, RespawnPlatform1, RespawnPlatform2, RespawnPlatform3, RespawnPlatform4;
     //[SerializeField] private Transform respawnPosition; // Position where the player should respawn
     //[SerializeField] private string playerTag = "Player"; // Tag used to identify the player
     public LevelManager levelManager;
@@ -21,6 +22,14 @@ public class Killbox : MonoBehaviour
     private void Start()
     {
         player = GetComponent<FirstPersonController>();
+        Platform1.SetActive(true);
+        Platform2.SetActive(true);
+        Platform3.SetActive(true);
+        Platform4.SetActive(true);
+        RespawnPlatform1.SetActive(false);
+        RespawnPlatform2.SetActive(false);
+        RespawnPlatform3.SetActive(false);
+        RespawnPlatform4.SetActive(false);
 
     }
     void OnTriggerEnter(Collider other)
@@ -30,6 +39,17 @@ public class Killbox : MonoBehaviour
             Debug.Log("collided with killbox");
             // Respawn the player to the respawn position
             levelManager.RespawnPlayer();
+
+            Platform1.SetActive(false);
+            Platform2.SetActive(false);
+            Platform3.SetActive(false);
+            Platform4.SetActive(false);
+            RespawnPlatform1.SetActive(true);
+            RespawnPlatform2.SetActive(true);
+            RespawnPlatform3.SetActive(true);
+            RespawnPlatform4.SetActive(true);
+
+
         }
     }
 }
