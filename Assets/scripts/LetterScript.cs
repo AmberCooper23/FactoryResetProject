2afac2bce4c946677fa44bfa2fceecb468fe46d5
timothychilezi20 <timothychilezi@gmail.com>
2024-09-20 05:54:15 +0200

    Doors Working Again With Reading Mechanic Too!

---
+++ Assets/scripts/LetterScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterScript : MonoBehaviour
+{
+    public GameObject letterUI;
+    bool toggle;
+
+    public void openCloseLetter()
+    {
+        toggle = !toggle;
+        if (toggle == false)
+        {
+            letterUI.SetActive(false);
+        }
+        if (toggle == true)
+        {
+            letterUI.SetActive(true);
+        }
+    }
+}
